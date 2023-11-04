86adde9c147e532ecc2dfe414c3b60f1260637a5
AidarhanKasenov <Tyuiu.KasenovAE@outlook.com>
2023-11-04 18:32:53 +0500

    Task1

---
+++ Tyuiu.KasenovAE.Sprint4.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KasenovAE.Sprint4.Task1.V2.Lib;
+
+namespace Tyuiu.KasenovAE.Sprint4.Task1.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Calculate_Test()
+        {
+            int[] arr = new int[] { 1, 7, 2, 2, 6, 2, 3, 2, 3, 5, 7, 7, 1, 2 };
+            DataService ds = new DataService();
+            Assert.AreEqual(ds.Calculate(arr), 34);
+        }
+    }
+}
---
+++ Tyuiu.KasenovAE.Sprint4.Task1.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KasenovAE.Sprint4.Task1.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KasenovAE.Sprint4.Task1.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a49eb381-f12b-43f3-9da7-be480bd24a34")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
