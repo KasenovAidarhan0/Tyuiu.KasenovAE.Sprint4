57e1ab558d77c0ddb74ae85fb2d6ee544269f995
AidarhanKasenov <Tyuiu.KasenovAE@outlook.com>
2023-11-08 21:40:37 +0500

    Task5

---
+++ Tyuiu.KasenovAE.Sprint4.Task5.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KasenovAE.Sprint4.Task5.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KasenovAE.Sprint4.Task5.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f6e8c5f8-dd49-4e44-83c0-3ac753b5fd5d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.KasenovAE.Sprint4.Task5.V11/Program.cs
+++ Tyuiu.KasenovAE.Sprint4.Task5.V11/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.KasenovAE.Sprint4.Task4.V14.Lib;
+using Tyuiu.KasenovAE.Sprint4.Task5.V11.Lib;
 
-namespace Tyuiu.KasenovAE.Sprint4.Task4.V14
+namespace Tyuiu.KasenovAE.Sprint4.Task5.V11
 {
     class Program
     {
@@ -14,41 +14,32 @@ namespace Tyuiu.KasenovAE.Sprint4.Task4.V14
             Console.Title = "Спринт #4 | Выполнил: Касенов А. Е. | ПКТб-23-2";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #4                                                               *");
-            Console.WriteLine("* Тема: Двумерные массивы (ввод с клавиатуры)                             *");
-            Console.WriteLine("* Задание #4                                                              *");
-            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
             Console.WriteLine("* Выполнил: Касенов Айдархан Есенаевич | ПКТб-23-2                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов,                    *");
-            Console.WriteLine("* заполненный значениями с клавиатуры в диапазоне от 1 до 8.              *");
-            Console.WriteLine("* Заменить нечетные элементы массива на 0.                                *");
-            Console.WriteLine("* 1, 3, 7, 7, 6,                                                          *");
-            Console.WriteLine("* 4, 2, 6, 3, 3,                                                          *");
-            Console.WriteLine("* 4, 5, 1, 4, 6,                                                          *");
-            Console.WriteLine("* 7, 5, 2, 7, 7,                                                          *");
-            Console.WriteLine("* 2, 4, 4, 4, 7,                                                          *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от -7 до 2. Заменить положительные    *");
+            Console.WriteLine("* элементы на 1.                                                          *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-
-            Console.WriteLine("Введите количество строк в массиве: ");
+            Console.Write("Введите количество строк в массиве: ");
             int rows = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите количество столбцов в массиве: ");
+            Console.Write("Введите количество столбцов в массиве: ");
             int columns = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("***************************************************************************");
+            Random rnd = new Random();
             int[,] mtrx = new int[columns, rows];
             for (int i = 0; i < columns; i++)
             {
                 for (int j = 0; j < rows; j++)
                 {
-                    Console.Write($"Введите {i}, {j} элемент массива: ");
-                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                    mtrx[i, j] = rnd.Next(-7, 2);
                 }
             }
-
-            Console.WriteLine("\nМассив:");
+            Console.WriteLine("Массив:");
             for (int i = 0; i < columns; i++)
             {
                 for (int j = 0; j < rows; j++)
@@ -57,11 +48,9 @@ namespace Tyuiu.KasenovAE.Sprint4.Task4.V14
                 }
                 Console.WriteLine();
             }
-
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-
             DataService ds = new DataService();
             mtrx = ds.Calculate(mtrx);
             for (int i = 0; i < columns; i++)
